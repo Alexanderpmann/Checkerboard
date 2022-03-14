800c02b4240dda8fb6cf866490caa4f3d5c4e4cd
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2022-03-13 22:15:40 -0400

    Add files via upload

--- Checkerboard/server.py
+++ Checkerboard/server.py
@@ -4,13 +4,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-
-
-
-
-
-
-
+@app.route('/4')
+def four():
+    return render_template("4.html")
 
 
 if __name__=="__main__":
